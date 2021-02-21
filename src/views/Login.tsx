4470acb6e52476d6590db4addb335bff83b37f74
kbxs106 <yaidi.garcia@astrazeneca.com>
2021-02-20 20:15:40 -0600

    adding login view with test

---
+++ src/views/Login.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from 'react';
+
+const Login = (): ReactElement => {
+  return (
+    <button type="button" className=".button-google">
+      Login with Google
+    </button>
+  );
+};
+export default Login;

5b2457a563c5fd9f227ea4d2b68198ed2400f46d
kbxs106 <yaidi.garcia@astrazeneca.com>
2021-02-21 13:19:37 -0600

    addign service to login with google

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -2,9 +2,9 @@ import React, { ReactElement } from 'react';
 
 const Login = (): ReactElement => {
   return (
-    <button type="button" className=".button-google">
-      Login with Google
-    </button>
+    <div>
+      <div className="g-signin2" data-onsuccess="onSignIn" />
+    </div>
   );
 };
 export default Login;

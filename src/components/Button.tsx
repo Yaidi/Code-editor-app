7e9a753fc23aa4780dd84db81d97d5c8c3084847
kbxs106 <yaidi.garcia@astrazeneca.com>
2021-02-20 22:34:44 -0600

    delete files not useful

--- src/components/Button.tsx
+++
@@ -1,7 +0,0 @@
-import React, { ReactElement } from 'react';
-import Button from '@material-ui/core/Button';
-
-const ButtonGeneric = (): ReactElement => {
-  return <Button variant="contained">Default</Button>;
-};
-export default ButtonGeneric;

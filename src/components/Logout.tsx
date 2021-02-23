a4e8646052761b1a4f52244118b884ea457c7ef7
kbxs106 <yaidi.garcia@astrazeneca.com>
2021-02-22 19:43:29 -0600

    Logout works

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,13 +1,13 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useContext } from 'react';
 import { Button } from '@material-ui/core';
+import AuthContext from '../router/AuthContext';
 
 const Logout = (): ReactElement => {
-  function signOut() {
+  const { setUser } = useContext(AuthContext);
+  const signOut = () => {
     const auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-  }
+    auth2.signOut().then(() => setUser(null));
+  };
   return <Button onClick={signOut}>Log out</Button>;
 };
 export default Logout;
